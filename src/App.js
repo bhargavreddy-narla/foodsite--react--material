4ce82987e9903cb83fc1ee1d28b09a16d5a87cf9
hcmay2021-bhargav <85004210+hcmay2021-bhargav@users.noreply.github.com>
2021-09-24 14:38:26 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button1 from "./23-09-2021/Code/Components/Button1/Button1";
+import Button2 from "./23-09-2021/Code/Components/Button2/Button2";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button1 />
+      <Button2 />
     </div>
   );
 }
